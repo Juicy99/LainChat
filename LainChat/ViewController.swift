d7df2d83fa10cd7d715e033354e42527a9d68c87
Juicy99 <juicy99@Juicy99noMacBook-Pro.local>
2021-02-20 21:57:41 +0900

    first commit

---
+++ LainChat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LainChat
+//
+//  Created by Juicy99 on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
