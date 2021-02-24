c2fd7e7304ec5ee78a38475fc23d8f96f9fbc437
Juicy99 <unot9777@gmail.com>
2021-02-25 07:08:50 +0900

    user add

--- LainChat/Controllers/SignUpViewController.swift
+++ LainChat/Controllers/SignUpViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var profileImageButton: UIButton!
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var registerButton: UIButton!
+    @IBAction func tappedProfileImageButton(_ sender: Any) {
+let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
     
     
     
@@ -22,8 +30,84 @@ class SignUpViewController: UIViewController {
         profileImageButton.layer.borderWidth = 1
         profileImageButton.layer.borderColor = UIColor.black.cgColor
         
-        registerButton.layer.cornerRadius = 15
+        registerButton.layer.cornerRadius = 12
+        
+        registerButton.addTarget(self, action: #selector(tappedRegisterButton), for:  .touchUpInside)
+        
+        userNameTextField.delegate = self
+        
+        registerButton.isEnabled = false
+        registerButton.backgroundColor = .gray
         
         
     }
+    
+    @objc private func tappedRegisterButton() {
+        Auth.auth().signInAnonymously() { (authResult, err) in
+            if let err = err {
+                print("認証情報の保存に失敗しました。\(err)")
+                return
+            }
+            print("認証情報の保存に成功しました。")
+
+            guard let user = authResult?.user else { return } // true
+            let uid = user.uid
+            guard let username = self.userNameTextField.text else { return }
+            let docData = [
+                "username": username,
+                "createdAt": Timestamp()
+                ] as [String : Any]
+            Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
+                if let err = err {
+                    print("Firestoreへの保存に失敗しました。\(err)")
+                    return
+                }
+                
+                print("Firestoreへの情報の保存が成功しました。")
+                self.dismiss(animated: true, completion: nil)
+                
+            }
+                   return
+               }
+            
+
+            
+        }
+        
+    }
+
+
+extension SignUpViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        print(" : ", textField.text)
+        let usernameIsEmpty = userNameTextField.text?.isEmpty ?? false
+        
+        if usernameIsEmpty{
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = .gray
+        } else{
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = .black
+        }
+    }
+}
+
+extension SignUpViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage{
+            profileImageButton.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            profileImageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        profileImageButton.setTitle("", for:  .normal)
+        profileImageButton.imageView?.contentMode = .scaleToFill
+        profileImageButton.contentHorizontalAlignment = .fill
+        profileImageButton.contentVerticalAlignment = .fill
+        profileImageButton.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
